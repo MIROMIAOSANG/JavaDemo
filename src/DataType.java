685ecd6128fd125713c9464335a31391704f0e1e
johnny <869743653@qq.com>
2018-04-03 11:53:03 +0800

    demo

---
+++ src/DataType.java
@@ -0,0 +1,22 @@
+/**
+ * @date 20180403
+ * this is a demo about fundamental data type
+ */
+public class DataType {
+    public static void main(String[] args) {
+        //长整型
+        long lg=400000000000000000L;
+        System.out.println(lg);
+        System.out.println();
+
+        //非十进制
+        int hex=0x14;
+        System.out.println(hex);
+        int binary=0b1001;
+        System.out.println(binary);
+        int oct=010;
+        System.out.println(oct);
+        System.out.println();
+
+    }
+}
