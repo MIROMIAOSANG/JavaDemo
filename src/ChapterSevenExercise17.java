8b8936d28feaaa76635075f6cc50ed0625d5c072
johnny <869743653@qq.com>
2018-04-26 22:57:07 +0800

    java practice

--- src/ChapterSevenExercise17.java
+++ src/ChapterSevenExercise17.java
@@ -5,16 +5,20 @@
 public class ChapterSevenExercise17 {
 
 }
+
 class Amphibian {
     protected void swim() {
         System.out.println("Amphibian swim");
     }
+
     protected void speak() {
         System.out.println("Amphibian speak");
     }
+
     void eat() {
         System.out.println("Amphibian eat");
     }
+
     static void grow(Amphibian a) {
         System.out.println("Amphibian grow");
         a.eat();
@@ -22,19 +26,26 @@ class Amphibian {
 }
 
 class Frog17 extends Amphibian {
-    @Override protected void swim() {
+    @Override
+    protected void swim() {
         System.out.println("Frog swim");
     }
-    @Override protected void speak() {
+
+    @Override
+    protected void speak() {
         System.out.println("Frog speak");
     }
-    @Override void eat() {
+
+    @Override
+    void eat() {
         System.out.println("Frog eat");
     }
+
     static void grow(Amphibian a) {
         System.out.println("Frog grow");
         a.eat();
     }
+
     public static void main(String[] args) {
         Frog17 f = new Frog17();
         // call overridden base-class methods:
