da190265990e8fe91abc6806078628681c215465
johnny <869743653@qq.com>
2018-05-24 22:59:27 +0800

    java practice

--- src/MaxSubSum.java
+++ src/MaxSubSum.java
@@ -2,8 +2,17 @@
  * 最大子列和
  */
 public class MaxSubSum {
-
-    public static void main(String[] args) {
-
+    public static int maxSubSum(int[] a) {
+        int thisSum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < a.length; i++) {
+            thisSum = thisSum + a[i];
+            if (thisSum > maxSum) {
+                maxSum = thisSum;
+            } else if (thisSum < 0) {
+                thisSum = 0;
+            }
+        }
+        return maxSum;
     }
 }
